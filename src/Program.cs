bc476657d0f57be49998be0001b9af77b7a96c82
MaximilianMcC <max@maximilian.co.nz>
2026-04-16 00:56:13 +1200

    erhm collision stuff kinda

--- src/Program.cs
+++ src/Program.cs
@@ -6,9 +6,9 @@ class Program
 	public static void Main(string[] args)
 	{
 		Raylib.SetTraceLogLevel(TraceLogLevel.Warning);
-		Raylib.InitWindow(500, 500, "raylib-cs gizmo example");
+		Raylib.InitWindow(800, 800, "raylib-cs gizmo example");
+		Raylib.SetExitKey(KeyboardKey.Null);
 
-		Raylib.DisableCursor();
 		Camera3D camera = new Camera3D()
 		{
 			Position = new Vector3(0.0f, 5.0f, 5.0f),
@@ -24,13 +24,27 @@ class Program
 		Transform transform = new Transform()
 		{
 			Translation = Vector3.Zero,
-			Rotation = Quaternion.Identity,
+			Rotation = Quaternion.CreateFromYawPitchRoll(45, 0, 0),
 			Scale = Vector3.One
 		};
 
+		bool local = false;
+
+		Raylib.DisableCursor();
+		bool useCamera = true;
+
 		while (Raylib.WindowShouldClose() == false)
 		{
-			Raylib.UpdateCamera(ref camera, CameraMode.Free);
+			if (useCamera) Raylib.UpdateCamera(ref camera, CameraMode.Free);
+
+			if (Raylib.IsKeyPressed(KeyboardKey.Space)) local = !local;
+			if (Raylib.IsKeyPressed(KeyboardKey.Escape))
+			{
+				useCamera = !useCamera;
+
+				if (useCamera) Raylib.DisableCursor();
+				else Raylib.EnableCursor();
+			}
 
 			Raylib.BeginDrawing();
 			Raylib.ClearBackground(Color.Black);
@@ -39,8 +53,12 @@ class Program
 			// model.Transform = RaylibGizmo.Matrix;
 			// Raylib.DrawModel(model, Vector3.Zero, 1f, Color.White);
 			// RaylibGizmo.DrawGizmo3D(ref model.Transform);
+
 			Raylib.DrawGrid(10, 1);
-			RaylibGizmo.DrawGizmo3D(ref transform);
+			// RaylibGizmo.DrawGizmo3D(ref transform);
+			RaylibGizmo.DrawGizmo3DPro(ref transform, local);
+
+			Raylib.DrawText("Press space to toggle between local and global", 10, 10, 16, Color.White);
 
 			Raylib.EndMode3D();
 			Raylib.EndDrawing();
--- src/RaylibGizmo.cs
+++ src/RaylibGizmo.cs
@@ -3,9 +3,9 @@ using Raylib_cs;
 
 class RaylibGizmo
 {
-	public static Color XColor { get; set; } = Color.Yellow;
+	public static Color XColor { get; set; } = Color.Red;
 	public static Color YColor { get; set; } = Color.Blue;
-	public static Color ZColor { get; set; } = Color.Red;
+	public static Color ZColor { get; set; } = Color.Yellow;
 	public static Color CenterColor { get; set; } = Color.RayWhite;
 
 	// Changes how large the thing is rendered visually or whatever
@@ -15,6 +15,8 @@ class RaylibGizmo
 	private static float axisLength => DrawScale * 0.5f;
 	private static float coneLength => DrawScale * 0.1f;
 	private static float coneBaseThickness => axisThickness * 2f;
+
+	public static bool HasPermissionToChangeCursor { get; set; } = true;
 
 	public static void SetColors(Color x, Color y, Color z, Color center)
 	{
@@ -31,13 +33,55 @@ class RaylibGizmo
 		// Draw the little centre thingy
 		Raylib.DrawSphereEx(transform.Translation, centerRadius, 6, 6, CenterColor);
 
-		// Draw the axes
-		DrawAxis(transform, Vector3.UnitX, XColor);
-		DrawAxis(transform, Vector3.UnitY, YColor);
-		DrawAxis(transform, Vector3.UnitZ, ZColor);
+		// Draw the axes globally
+		DrawAxisGlobal(transform, Vector3.UnitX, XColor);
+		DrawAxisGlobal(transform, Vector3.UnitY, YColor);
+		DrawAxisGlobal(transform, Vector3.UnitZ, ZColor);
 	}
 
-	private static void DrawAxis(Transform transform, Vector3 axis, Color color)
+	public static void DrawGizmo3DPro(ref Transform transform, bool local)
+	{
+		// Draw the little centre thingy
+		Raylib.DrawSphereEx(transform.Translation, centerRadius, 6, 6, CenterColor);
+
+		if (local)
+		{
+			DrawAxisLocal(transform, Vector3.UnitX, XColor);
+			DrawAxisLocal(transform, Vector3.UnitY, YColor);
+			DrawAxisLocal(transform, Vector3.UnitZ, ZColor);
+		}
+		else
+		{
+			DrawAxisGlobal(transform, Vector3.UnitX, XColor);
+			DrawAxisGlobal(transform, Vector3.UnitY, YColor);
+			DrawAxisGlobal(transform, Vector3.UnitZ, ZColor);
+		}
+
+		CheckForAxisCollision(transform, Vector3.UnitX);
+	}
+
+	// TODO: Don't just copy paste all this for local/global
+	private static void DrawAxisGlobal(Transform transform, Vector3 axis, Color color)
+	{
+		// Make a copy of the transform so we can modify it temporarily
+		Transform start = transform;
+		Transform end = transform;
+
+		// Add a bit of padding from the centre
+		start.TranslateGlobal(axis * (centerRadius * 2f));
+
+		// Draw the tube thing
+		end.TranslateGlobal(axis * axisLength);
+		Raylib.DrawCylinderEx(start.Translation, end.Translation, axisThickness, axisThickness, 8, color);
+
+		// Draw the cone
+		Transform coneStart = end;
+		end.TranslateGlobal(axis * coneLength);
+		Raylib.DrawCylinderEx(coneStart.Translation, end.Translation, coneBaseThickness, 0f, 8, color);
+	}
+
+	// TODO: Don't just copy paste all this for local/global
+	private static void DrawAxisLocal(Transform transform, Vector3 axis, Color color)
 	{
 		// Make a copy of the transform so we can modify it temporarily
 		Transform start = transform;
@@ -54,5 +98,35 @@ class RaylibGizmo
 		Transform coneStart = end;
 		end.TranslateLocal(axis * coneLength);
 		Raylib.DrawCylinderEx(coneStart.Translation, end.Translation, coneBaseThickness, 0f, 8, color);
+	}
+
+	//! This will NOT work for local/rotated ones. Maybe use like collisions instead idk
+	private static void CheckForAxisCollision(Transform transform, Vector3 axis)
+	{
+		// Make a hitbox that's roughly the size of the axis
+		float boxSize = coneBaseThickness * 2f;
+
+		Vector3 start = transform.Translation + axis * (centerRadius * 2f);
+		Vector3 end = transform.Translation + axis * ((centerRadius * 2f) + axisLength + coneLength);
+		Vector3 thickness = new Vector3(boxSize);
+
+		BoundingBox hitbox = new BoundingBox(
+			Vector3.Min(start, end) - thickness,
+			Vector3.Max(start, end) + thickness
+		);
+
+		//! debug draw
+		Raylib.DrawCubeWiresV(hitbox.Min, hitbox.Max, Color.Magenta);
+
+		// Raycast to check for if it's being clicked on
+	}
+
+	// TODO: DO this
+	class GizmoAxis
+	{
+		//?	CheckForCollision()
+		//?	CheckForDragging()
+		//? DrawGlobal()
+		//? DrawLocal()
 	}
 }
